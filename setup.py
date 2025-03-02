82fded438685f2d6e49c406597a9c7387761e251
Rett Graham <rettg@umich.edu>
2025-03-02 10:56:29 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your_project",
+    version="0.1.0",
+    author='Rett Graham',
+    packages=find_packages(),
+    url='https://github.com/rettag/leetcode',
+    install_requires=[
+        "pytest",
+    ],
+)
